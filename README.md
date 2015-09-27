f0cdeb242d6578e4c518bde16cb9af9be418e70f
rameshs <rameshs@captricity.com>
2015-09-27 14:05:53 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# My dotfiles for easy installation on a new machine
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env zsh
+
+### Set up zsh (prezto)
+git clone --recursive https://github.com/rameshvs/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+chsh -s /bin/zsh
+
+### Set up vim
+
+git clone --recursive http://github.com/rameshvs/dotvim $HOME/.vim
+ln -s $HOME/.vim/vimrc .vimrc
+touch $HOME/.vim/viminfo
+ln -s $HOME/.vim/viminfo $HOME/.viminfo
+
+### set up other stuff
+cp tmux.conf ~/.tmux.conf
